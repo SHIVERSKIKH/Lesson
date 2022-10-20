4cfcc67f1600af7a6fa4daec9f2130635d97dac7
shiverskikh <katerina_kononenko_94@mail.ru>
2022-10-20 16:38:28 +0700

    Add lesson one

---
+++ Lesson_one/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+num = num * 2;
+
+Console.WriteLine("Your number is " + num);*/
+
+/*Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 *n2;
+
+if(n1 == quad2) 
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("no");
+}*/
+
+//Задача 2.
+//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*Console.Write("Input a positive int integer number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while(current<=n)
+{
+    Console.Write(current + " ");
+    current++;
+}*/
+
+//Задача3.
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+/*Console.Write("Input a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+Console.WriteLine($" Lust digit of {num} is {result}");*/ 
