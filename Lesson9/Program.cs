12c01a9257e84a2d5d097f4dda022db9b5654ea1
shiverskikh <katerina_kononenko_94@mail.ru>
2022-11-24 15:27:17 +0700

    Lesson add  9

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
 /*
-void ShowNumbers(int n)
+void ShowNumbers(double n)
 {
     if(n > 1) ShowNumbers(n - 1);
     Console.Write(n + " ");
@@ -10,7 +10,7 @@ ShowNumbers(5);
 */
 //Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 /*
-int SumOfDigits(int num)
+double SumOfDigits(double num)
 {
     if(num != 0) return SumOfDigits(num / 10) + num % 10;
     return 0;
@@ -20,13 +20,13 @@ Console.WriteLine(SumOfDigits(1234));
 */
 //Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 /*
-void ShowNumbers(int m, int n)
+void ShowNumbers(double m, double n)
 {
-    if(m > n) 
+    if(m > n)
     {   ShowNumbers (m - 1, n);
         Console.Write(m + " ");
     }
-    else 
+    else
     {   if(n > m) ShowNumbers(n - 1, m);
         Console.Write(n + " ");
     }
@@ -36,13 +36,16 @@ ShowNumbers(5, 5);
 */
 //Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
 
-int PowNumbers(int a, int b)
+double PowNumbers(double a, double b)
 {
-    if(b != 0) return PowNumbers(a, b - 1) * a; 
+    if (b > 0)
+        return PowNumbers(a, b - 1) * a;
+    if (b < 0)
+        return PowNumbers(a, b + 1) / a;
     return 1;
 }
 Console.WriteLine("Input a number A: ");
-int a = Convert.ToInt32(Console.ReadLine());
+double a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Input a number B: ");
-int b = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine(PowNumbers(a, b));
